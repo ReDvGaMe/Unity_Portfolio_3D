5581ce42434cd02a57cd095362b200f328561543
Kim Ju-Yeong <32252062+ReDvGaMe@users.noreply.github.com>
2018-11-22 16:57:17 +0900

    애니메이션 변경 중

--- Assets/#Script/Controller/PlayerController.cs
+++ Assets/#Script/Controller/PlayerController.cs
@@ -6,15 +6,21 @@ public class PlayerController : MonoBehaviour {
 	#region 변수
 	private Rigidbody _rigid;
 	private Animator _anim;
+	// public CharacterController _characterController;
 
+	#region 정보
+	[Tooltip("캐릭터 기본 이동 속도")] [SerializeField] [Range(0f, 10f)]
+	private float _basicMoveSpeed = 10f;
+	// 캐릭터 이동 속도
+	private float _moveSpeed = 10f;
+	#endregion
+
+	#region 입력 키
 	// 수직 키 입력값
 	private float _keyVertical = 0f;
 
 	// 수평 키 입력값
 	private float _keyHorizontal = 0f;
-
-	[Tooltip("캐릭터 이동속도")] [SerializeField] [Range(0f, 10f)]
-	private float _moveSpeed = 8f;
 
 	// 달리기 키
 	private KeyCode _runKey = KeyCode.LeftShift;
@@ -22,11 +28,17 @@ public class PlayerController : MonoBehaviour {
 	// 점프 키
 	private KeyCode _jumpKey = KeyCode.Space;
 
+	// 앉기 키
+	private KeyCode _crouchKey = KeyCode.LeftControl;
+	#endregion
+
+	#region 상태
 	// 달리고있는지 검사할 변수
 	private bool _runState = false;
 
 	// 캐릭터가 공중에 있는지 검사할 변수
 	private bool _jumpState = false;
+	#endregion
 	#endregion
 
 	#region 기본 함수
@@ -34,6 +46,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		_rigid = GetComponent<Rigidbody>();
 		_anim = transform.GetComponentInChildren<Animator>();
+		// _characterController = gameObject.GetComponent<CharacterController>();
 	}
 
 	// Use this for initialization
@@ -44,7 +57,7 @@ public class PlayerController : MonoBehaviour {
 
 	private void FixedUpdate()
 	{
-		
+		MovePosition();
 	}
 
 	// Update is called once per frame
@@ -59,8 +72,7 @@ public class PlayerController : MonoBehaviour {
 		_keyVertical = Input.GetAxis("Vertical");
 		_keyHorizontal = Input.GetAxis("Horizontal");
 
-		Debug.Log(_keyVertical);
-
+		// 달리기 키 검사
 		if (Input.GetKey(_runKey))
 		{
 			if (_keyVertical > 0f) _runState = true;
@@ -75,11 +87,14 @@ public class PlayerController : MonoBehaviour {
 			_anim.SetBool("_Run", _runState);
 		}
 
-		_anim.SetFloat("_Speed", _keyVertical);
+		if (_runState) _moveSpeed = _basicMoveSpeed * 2;
+		else _moveSpeed = _basicMoveSpeed;
 
-		if (Input.GetKeyDown(_jumpKey))
+		_anim.SetFloat("_Speed", Mathf.Abs(_keyVertical));
+
+		if (Input.GetKeyDown(_jumpKey) && !(_jumpState))
 		{
-			_rigid.AddForce(Vector3.up * 30f, ForceMode.Impulse);
+			//_rigid.AddForce(Vector3.up * 10f, ForceMode.Impulse);
 			_anim.SetTrigger("_Jump");
 		}			
 	}
@@ -89,7 +104,9 @@ public class PlayerController : MonoBehaviour {
 	// 이동 제어
 	private void MovePosition()
 	{
-		_rigid.AddForce(transform.rotation * Vector3.forward);
+		//transform.Translate(Vector3.forward * _keyVertical * _moveSpeed * Time.deltaTime);
+		//_rigid.AddForce(transform.rotation * Vector3.forward * _keyVertical * _moveSpeed);
+		//_characterController.Move(Vector3.forward * _keyVertical * _moveSpeed);
 	}
 
 	// 회전 제어
@@ -98,4 +115,20 @@ public class PlayerController : MonoBehaviour {
 
 	}
 	#endregion
+
+	private void OnCollisionStay(Collision collision)
+	{
+		if(collision.transform.tag == "Ground") // Ground
+		{
+			if (_jumpState) _jumpState = false;
+		}
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		if(collision.transform.tag == "Ground") // Ground
+		{
+			if (!_jumpState) _jumpState = true;
+		}
+	}
 }
