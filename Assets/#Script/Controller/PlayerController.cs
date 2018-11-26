d286a30a9ab12930e56d1c0e9df0b21d73476c49
Kim Ju-Yeong <32252062+ReDvGaMe@users.noreply.github.com>
2018-11-26 16:51:26 +0900

    애니메이션 변경 중

--- Assets/#Script/Controller/PlayerController.cs
+++ Assets/#Script/Controller/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour {
 	private float _basicMoveSpeed = 10f;
 	// 캐릭터 이동 속도
 	private float _moveSpeed = 10f;
+	// 캐릭터 이동 관련 벡터
+	private Vector3 movement;
 	#endregion
 
 	#region 입력 키
@@ -30,6 +32,9 @@ public class PlayerController : MonoBehaviour {
 
 	// 앉기 키
 	private KeyCode _crouchKey = KeyCode.LeftControl;
+
+	// 카메라 회전 키
+	private KeyCode _cameraMoveKey = KeyCode.LeftAlt;
 
 	// 마우스
 	private Vector3 _mousePos;
@@ -54,6 +59,7 @@ public class PlayerController : MonoBehaviour {
 
 	private void FixedUpdate()
 	{
+		MoveCharacter();
 		RotateCharacter();
 	}
 
@@ -85,10 +91,19 @@ public class PlayerController : MonoBehaviour {
 			_anim.SetBool("_Run", _runState);
 		}
 
-		// if (_runState) _moveSpeed = _basicMoveSpeed * 2;
-		// else _moveSpeed = _basicMoveSpeed;
+		if (_runState) _moveSpeed = _basicMoveSpeed * 2;
+		else _moveSpeed = _basicMoveSpeed;
 
+		// 방향 키 입력을 애니메이터로 넘김
+		if (Mathf.Abs(_keyVertical) > 0)
+			_anim.SetBool("_MoveVertical", true);
+		else
+			_anim.SetBool("_MoveVertical", false);
 		_anim.SetFloat("_SpeedVertical", _keyVertical);
+		if (Mathf.Abs(_keyHorizontal) > 0)
+			_anim.SetBool("_MoveSide", true);
+		else
+			_anim.SetBool("_MoveSide", false);
 		_anim.SetFloat("_SpeedHorizontal", _keyHorizontal);
 
 		if (Input.GetKeyDown(_jumpKey) && !(_jumpState))
@@ -100,6 +115,20 @@ public class PlayerController : MonoBehaviour {
 	#endregion
 
 	#region 캐릭터 이동 관련 함수
+	// 이동 제어
+	private void MoveCharacter()
+	{
+		AnimatorStateInfo animInfo = _anim.GetCurrentAnimatorStateInfo(0);
+
+		if (!(_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")))
+		{
+			StartCoroutine("WaitForSec", 0.2f);
+			movement.Set(_keyHorizontal, 0, _keyVertical);
+			movement = movement.normalized * _basicMoveSpeed * Time.deltaTime;
+
+			_rigid.MovePosition(transform.position + movement);
+		}		
+	}
 	// 회전 제어
 	private void RotateCharacter()
 	{
@@ -122,4 +151,15 @@ public class PlayerController : MonoBehaviour {
 			if (!_jumpState) _jumpState = true;
 		}
 	}
+
+	#region 코루틴
+	private IEnumerator WaitForSec(float time)
+	{
+		while(time > 0)
+		{
+			time -= 0.1f;
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+	#endregion
 }
